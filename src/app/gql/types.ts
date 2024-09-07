b3959c4f19e8dbaea6f1adbd28bcb523bb2b8a85
Adam Crume <acrume@google.com>
2024-09-07 16:53:57 -0700

    Implement toString for types

--- src/app/gql/types.ts
+++ src/app/gql/types.ts
@@ -16,6 +16,7 @@
 
 export interface TypeBase {
   kind: string;
+  toString(): string;
 }
 
 export interface AnyType extends TypeBase {
@@ -24,52 +25,74 @@ export interface AnyType extends TypeBase {
 
 export const ANY: AnyType = {
   kind: 'any',
+  toString() {
+    return 'ANY';
+  },
 };
 
 export interface BooleanType extends TypeBase {
   kind: 'boolean';
 }
 
-export const BOOLEAN: BooleanType = { kind: 'boolean' };
+export const BOOLEAN: BooleanType = {
+  kind: 'boolean',
+  toString() {
+    return 'BOOLEAN';
+  },
+};
 
 export interface StringType extends TypeBase {
   kind: 'string';
 }
 
-export const STRING: StringType = { kind: 'string' };
+export const STRING: StringType = {
+  kind: 'string',
+  toString() {
+    return 'STRING';
+  },
+};
 
 export interface NumberType extends TypeBase {
   kind: 'number';
 }
 
-export const NUMBER: NumberType = { kind: 'number' };
+export const NUMBER: NumberType = {
+  kind: 'number',
+  toString() {
+    return 'NUMBER';
+  },
+};
 
 export interface ListType extends TypeBase {
   kind: 'list';
   inner: Type;
 }
 
-export const NUMBER_LIST: ListType = {
-  kind: 'list',
-  inner: NUMBER,
-};
+export const NUMBER_LIST: ListType = listOf(NUMBER);
 
-export const ANY_LIST: ListType = {
-  kind: 'list',
-  inner: ANY,
-};
+export const ANY_LIST: ListType = listOf(ANY);
 
 export interface NodeRefType extends TypeBase {
   kind: 'node_ref';
 }
 
-export const NODE_REF: NodeRefType = { kind: 'node_ref' };
+export const NODE_REF: NodeRefType = {
+  kind: 'node_ref',
+  toString() {
+    return 'NODE';
+  },
+};
 
 export interface EdgeRefType extends TypeBase {
   kind: 'edge_ref';
 }
 
-export const EDGE_REF: EdgeRefType = { kind: 'edge_ref' };
+export const EDGE_REF: EdgeRefType = {
+  kind: 'edge_ref',
+  toString() {
+    return 'EDGE';
+  },
+};
 
 export type Type =
   | AnyType
@@ -79,6 +102,16 @@ export type Type =
   | NodeRefType
   | NumberType
   | StringType;
+
+export function listOf(element: Type): ListType {
+  return {
+    kind: 'list',
+    inner: element,
+    toString() {
+      return `LIST<${element}>`;
+    },
+  };
+}
 
 export function typesEqual(a: Type, b: Type): boolean {
   if (a.kind === 'list' && b.kind === 'list') {
